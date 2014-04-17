c088fc0d9116886a0a346b8d10364209a6714634
Brittany Storoz <brittanystoroz@gmail.com>
2014-04-17 13:46:15 -0400

    Stub plugin js

---
+++ js/template-editor.js
@@ -0,0 +1,27 @@
+/*
+ * Template Editor - jQuery / Zepto theme/mockup editor plugin
+ *
+ * Copyright (c) 2014 Brittany Storoz
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   http://www.brittanystoroz.github.io/template-editor
+ *
+ * Version: 0.0.0
+ *
+ */
+
+;
+(function($, window, document, undefined) {
+
+  "use strict";
+
+  $.fn.templateditor = function(forks, opts) {
+
+    // dewe things
+
+  }; // end $.fn.templateditor
+
+})(window.jQuery || window.Zepto, window, document);
